3f30f340bb7a4657849c3a198b5ebe7de87b4d74
tsizo <ee180485@n-fukushi.ac.jp>
2021-04-25 14:45:30 +0900

    ログイン、ログアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
- validates :nickname, presense:true
- validates :family_name, presense:true
- validates :first_name, presense:true
- validates :family_name_kana, presense:true
- validates :first_name_kana, presense:true
- validates :birth_day, presense:true
+ validates :nickname, presence:true
+ validates :family_name, presence:true
+ validates :first_name, presence:true
+ validates :family_name_kana, presence:true
+ validates :first_name_kana, presence:true
+ validates :birth_day, presence:true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
