db86c46fe6a931b57c7214a518daed770fbbe1d2
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-01 14:35:15 +0900

    with_optionsでまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
 with_options presence:true do
  validates :nickname
- validates :family_name, format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
- validates :first_name, format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
- validates :family_name_kana,format:{with:/\A[ァ-ヶー－]+\z/}
- validates :first_name_kana,format:{with:/\A[ァ-ヶー－]+\z/}
+ validates :family_name_kana
+ validates :first_name_kana
  validates :birth_day
  validates :password,format:{with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+ with_options format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+ validates :family_name
+ validates :first_name
+end
+with_options format:{with:/\A[ァ-ヶー－]+\z/} do
+validates :family_name_kana
+ validates :first_name_kana
+end
 end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
