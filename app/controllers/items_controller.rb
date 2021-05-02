d8079276951d64aae7326ab6bf138da8442bd4a0
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-02 15:13:46 +0900

    モデルとテーブルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
   def index
     
   end
-  #def new
-  #@item = Item.new
-  #end
- # def create
+  def new
+  @item = Item.new
+  end
+ def create
     #Item.save(item_params)
-  #end
-#end
-#private
+  end
+end
+private
 
-#def item_params
- # params.require(:user).permit(:nickname, :birth_day, :first_name, :family_name, :first_name_kana, :family_name_kana).merge(user_id:current_user.id)
-#end
+def item_params
+  params.require(:user).permit(:nickname, :birth_day, :first_name, :family_name, :first_name_kana, :family_name_kana).merge(user_id:current_user.id)
+end
---
+++ app/models/item.rb
@@ -0,0 +1,15 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence :true do
+  validates :name
+  validates :category_id
+  validates :status_id
+  validates :shipping_cost_id
+  validates :shipping_days_id
+  validates :prefecture_id
+  validates :price、format:{with:/\A[0-9]+\z/}
+  validates :introduction
+  validates :user
+  end
+has_one_attached :image
+end
