d75defd81ec5aedab517beedbb475ab16e9da4c7
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-08 15:19:52 +0900

    javascript

---
+++ app/javascript/price.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+  console.log("OK");
+});
+const priceInput = document.getElementById("item-price");
+console.log(priceInput);
+const salesCommission = document.getElementById("add-tax-price");
+console.log(salesCommission);
+const profit = document.getElementById("profit");
+console.log(profit);
