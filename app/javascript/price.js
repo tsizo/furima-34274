7f2b9b2a5044cc35822959d803974af905e86a2f
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-09 19:05:55 +0900

    修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,11 +3,9 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
 const inputValue = priceInput.value;
-  console.log(inputValue);
 
   const salesCommission = document.getElementById("add-tax-price");
 salesCommission.innerHTML= Math.floor(inputValue*0.1)
-console.log(salesCommission);
 
 const profitNumber = document.getElementById("profit");
 const result=Math.floor(inputValue*0.1)
