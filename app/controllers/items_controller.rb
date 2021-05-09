8d7617b75beea399855b4ab940d779547ba7a5f9
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-09 18:55:03 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,17 +6,17 @@ class ItemsController < ApplicationController
   def new
   @item = Item.new
   end
- def create
+  def create
   @item=Item.new(item_params)
   if @item.save
-    redirect_to root_path(@item)
+  redirect_to root_path
   else
-   render :new
+  render :new
   end
-end
+  end
 private
 
-def item_params
-  params.require(:item).permit(:name,:image,:introduction,:category_id, :status_id, :shipping_cost_id, :shipping_days_id, :prefecture_id, :price).merge(user_id: current_user.id)
-end
-end
+ def item_params
+ params.require(:item).permit(:name,:image,:introduction,:category_id, :status_id, :shipping_cost_id, :shipping_days_id, :prefecture_id, :price).merge(user_id: current_user.id)
+ end
+ end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  before do
-    @item = FactoryBot.build(:item)
-  end
-  describe'出品機能' do
-  context'出品できるとき'do
-  it'全ての項目を入力すれば出品できる'do
-  expect(@item).to be_valid
+before do
+@item = FactoryBot.build(:item)
+end
+describe'出品機能' do
+context'出品できるとき'do
+it'全ての項目を入力すれば出品できる'do
+expect(@item).to be_valid
 end
 end
 context'出品できないとき' do
@@ -22,7 +22,7 @@ it"categoryが空だと出品できない"do
 expect(@item.errors.full_messages).to include("Category can't be blank")
 end
 it"categoryが_idが1だと出品できない"do
-@item.category_id ='1'
+@item.category_id =1
 @item.valid?
 expect(@item.errors.full_messages).to include("Category must be other than 1")
 end
@@ -32,7 +32,7 @@ it"statusが空だと出品できない"do
 expect(@item.errors.full_messages).to include("Status can't be blank")
 end
 it"status_idが1だと出品できない"do
-@item.status_id ='1'
+@item.status_id =1
 @item.valid?
 expect(@item.errors.full_messages).to include("Status must be other than 1")
 end
@@ -52,7 +52,7 @@ it"shipping_daysが空だと出品できない"do
 expect(@item.errors.full_messages).to include("Shipping days can't be blank")
 end
 it"shipping_days_idが1だと出品できない"do
-@item.shipping_days_id ='1'
+@item.shipping_days_id = 1
 @item.valid?
 expect(@item.errors.full_messages).to include("Shipping days must be other than 1")
 end
@@ -62,7 +62,7 @@ it"prefectureが空だと出品できない"do
 expect(@item.errors.full_messages).to include("Prefecture can't be blank")
 end
 it"prefecture_idが1だと出品できない"do
-@item.prefecture_id ='1'
+@item.prefecture_id = 1
 @item.valid?
 expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
 end
@@ -78,12 +78,12 @@ it'priceが全角数字だと出品できない'do
 expect(@item.errors.full_messages).to include("Price is not a number")
 end
 it'priceが¥300未満だと出品できない'do
-@item.price = '299'
+@item.price = 299
 @item.valid?
 expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
 end
 it'priceが10,000,000以上では出品できないこと'do
-@item.price = '10000007'
+@item.price = 10000007
 @item.valid?
 expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
 end
@@ -108,9 +108,9 @@ it'imageが空だと出品できない' do
 expect(@item.errors.full_messages).to include("Image can't be blank")
 end
 it '紐づくユーザーが存在しないと保存できないこと' do
-  @item.user = nil
-  @item.valid?
-  expect(@item.errors.full_messages).to include('User must exist')
+@item.user = nil
+@item.valid?
+expect(@item.errors.full_messages).to include('User must exist')
 end
 end
 end
