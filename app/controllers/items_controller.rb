6dbfe3153d0a8264d25ad46d7f23ca34db80f793
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-18 21:52:35 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,11 +17,23 @@ class ItemsController < ApplicationController
     render :new
    end
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
   
+  def edit
+    @item = Item.find(params[:id])
+  end  
+  
+  def update
+    @item = Item.find(params[:id])
+   if @item.update(item_params)
+    redirect_to item_path(@item.id)
+   else
+    render :edit
+   end
+  end
   private
 
   def item_params
