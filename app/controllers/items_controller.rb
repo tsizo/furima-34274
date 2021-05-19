30635fff997499c7f118463f4e73e8e959596854
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-19 11:26:46 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create,:edit, :update]
-  before_action :move_to_index,      only: [:edit]
+  before_action :move_to_index,      only: [:edit,:update]
   before_action :set_item,           only: [:show, :edit, :update, :move_to_index]
   def index
     @items = Item.all.order("created_at ASC") 
