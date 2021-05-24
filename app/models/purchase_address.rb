2ecbf0affbbe197097888b31f0ba9d79041bc994
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-25 02:00:51 +0900

    formオブジェクトの作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,16 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code,:prefecture_id, :city, :address, :building_name, :phone_number, :purchase_id 
+    with_options  presence:true do
+     validates :post_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/
+     validates :city
+     validates :address
+     validates :building_name
+     validates :phone_number,format: {with: /\A[0-9]{11}\z/ }
+     validates :prefecture_id, numericality:{other_than: 1}
+    end       
+ def save
+  purchase = Purchase.create(user_id:user_id,item_id:item_id)
+  Address.create(post_code:post_code,prefecture_id:prefecture_id, city:city, address:address, building_name:building_name, phone_number:phone_number, purchase_id:purchase_id )
+ end
+end
