339144ad92d3f9abf5f5b3748732db84baba13e3
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-26 03:49:40 +0900

    テストコード実装

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :purchase do
-    
+   post_code    {123-4567}
+   prefecture_id{2}
+   city         {'wed'} 
+   address      {'edf'}
+   phone_number {12345678912} 
   end
 end
