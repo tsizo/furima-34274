16afdac3d9604bd503c6eb63e0819aaa03f0f692
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-28 16:43:41 +0900

    エラーメッセージの出力

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,23 @@
 class PurchasesController < ApplicationController
   def index
-    @purchases = Purchase.all
+    @purchases = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @purchases = PurchaseAddress.new(purchase_params)
+    @item = Item.find(params[:item_id])
+    if @purchases.valid?
+      @purchases.save
+      redirect_to root_path  
+    else
+      render :index
+    end  
   end  
-end
 private
   def purchase_params
-    params.permit(:post_code,:prefecture_id,:city,:address,:building_name,:phone_number)
+    params.permit(:post_code,:prefecture_id,:city,:address,:building_name,:phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
   end
+end
+ 
+
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,7 +2,7 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code,:prefecture_id, :city, :address, :building_name, :phone_number, :purchase_id 
     with_options  presence:true do
-     validates :post_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/
+     validates :post_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
      validates :city
      validates :address
      validates :building_name
@@ -11,6 +11,6 @@ class PurchaseAddress
     end       
  def save
   purchase = Purchase.create(user_id:user_id,item_id:item_id)
-  Address.create(post_code:post_code,prefecture_id:prefecture_id, city:city, address:address, building_name:building_name, phone_number:phone_number, purchase_id:purchase_id )
+  Address.create!(post_code:post_code,prefecture_id:prefecture_id, city:city, address:address, building_name:building_name, phone_number:phone_number,purchase_id:purchase.id)
  end
 end
