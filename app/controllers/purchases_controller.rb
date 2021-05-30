08b692c6555ccce014b0e214ae26c2b8af80c0c5
tsizo <ee180485@n-fukushi.ac.jp>
2021-05-30 15:35:17 +0900

    コントローラー編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,13 @@
 class PurchasesController < ApplicationController
-
+ before_action :authenticate_user!, only: [:index,:create]
+ before_action :set_purchase,       only: [:index,:create]
+ before_action :move_to_root_path,  only: [:index,:create]
   def index
     @purchase = PurchaseAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @purchase = PurchaseAddress.new(purchase_params)
-    @item = Item.find(params[:item_id])
     if @purchase.valid?
       pay_item
       @purchase.save
@@ -20,6 +20,16 @@ private
   def purchase_params
     params.require(:purchase_address).permit(:post_code,:prefecture_id,:city,:address,:building_name,:phone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
   end
+
+  def set_purchase
+    @item = Item.find(params[:item_id])
+  end 
+  
+  def move_to_root_path
+    unless current_user.id == @item.user_id
+      redirect_to rooy_path
+  end
+
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
