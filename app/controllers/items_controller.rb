d8eb41a40a5bc9ea778d482d9d799ffe7d1b224c
tsizo <ee180485@n-fukushi.ac.jp>
2021-06-05 16:55:27 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index,      only: [:edit,:update,:destroy]
   
   def index
-    @items = Item.all.order("created_at ASC") 
+    @items = Item.all.includes(:user).order("created_at DESC") 
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,17 @@
 class User < ApplicationRecord
-with_options presence:true do
+ with_options presence:true do
  validates :nickname
- validates :family_name_kana
- validates :first_name_kana
  validates :birth_day
- validates :password,format:{with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
  with_options format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
  validates :family_name
  validates :first_name
-end
-with_options format:{with:/\A[ァ-ヶー－]+\z/} do
+ with_options format:{with:/\A[ァ-ヶー－]+\z/} do
 validates :family_name_kana
  validates :first_name_kana
+ end
+ end
 end
-end
+validates :password,format:{with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end       
