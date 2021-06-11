7ebc7a8537ad387d07ba959a2aac69d3b9f08cf8
tsizo <ee180485@n-fukushi.ac.jp>
2021-06-12 02:18:27 +0900

    自動デプロイ

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+lock "~> 3.1"
+set :application, 'furima-34274'
+
+set :repo_url,  'git@github.com:tsizo/furima-34274.git'
+
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5' 
+
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/sample.pem'] 
+
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
